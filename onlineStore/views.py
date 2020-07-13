900ea03f45d023ae0405455ee48c7fe4585cebbc
LuisFernando <lquispepum@unsa.edu.pe>
2020-07-13 16:24:20 -0500

    Haciendo un mejor funcionamiento del form y haciendo que los usuarios no se repitan

--- onlineStore/views.py
+++ onlineStore/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
 from .forms import CustomerForm
 from .models import Customer
+from  django.contrib import messages
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
@@ -14,15 +16,31 @@ def category(request):
 	return render(request, 'category.html',{})
 def createAccount(request):
 	form = CustomerForm()
-	if request.method == 'POST':
-		form = CustomerForm(request.POST)
-		if form.is_valid():
-			print(form.cleaned_data)
-			Customer.objects.create(**form.cleaned_data)
-		else :
-			print(form.errors)
-
-	context = {
-		'form' :form,
+	context ={
+		'form':form,
 	}
-	return render(request, 'createAccount.html',context)
+	if request.method == 'POST':					
+		dni = request.POST['dni']
+		email = request.POST['email']        
+		password1 = request.POST['password']
+		password2 = request.POST['password2']
+
+		if password1 == password2:
+			if Customer.objects.filter(email=email).exists():
+				messages.info(request,'Username Taken')
+				return redirect('/createAccount')
+			elif Customer.objects.filter(dni=dni).exists():
+				messages.info(request,'DNI Taken')
+				return redirect('/createAccount')
+			else:
+				form = CustomerForm(request.POST)
+				if form.is_valid():										
+					print(form.cleaned_data)
+					Customer.objects.create(**form.cleaned_data)
+					return redirect("/")				
+		else:
+			messages.info(request,'Password Not Matching')
+			return redirect('/createAccount')						
+	else:
+		return render(request, 'createAccount.html',context)
+	
