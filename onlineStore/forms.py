8bf820c640d1bf0dfe622f4507b372d6d570adad
LuisFernando <lquispepum@unsa.edu.pe>
2020-07-13 16:22:59 -0500

    cambiando algunos campos y añadiendo widgets

--- onlineStore/forms.py
+++ onlineStore/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from .models import Customer
 
 class CustomerForm(forms.Form):
-	name = forms.CharField()
-	last_name = forms.CharField()
-	dni = forms.IntegerField()
-	email = forms.EmailField()
-	password = forms.CharField()
+	name = forms.CharField(label= 'Nombre',widget=forms.TextInput(attrs={'placeholder': 'Ingresa tu Nombre'}))
+	last_name = forms.CharField(label = 'Apellidos',widget=forms.TextInput(attrs={'placeholder': 'Ingresa tu Apellido'}))
+	dni = forms.IntegerField(label = 'Ingrese su DNI',widget=forms.NumberInput(attrs={'placeholder': 'Ingrese su DNI'}))
+	email = forms.EmailField(label = 'Correo Electronico',widget=forms.EmailInput(attrs={'placeholder': 'example@email.com'}))
+	password = forms.CharField(label = 'Contraseña', widget = forms.PasswordInput(attrs={'placeholder': 'Ingresa tu Contraseña'}))
